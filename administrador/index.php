61708db44660a7e3094ea962eff372e3bbf3fb48
CanutoIS <canutosorianoivan@gmail.com>
2024-03-02 18:05:46 +0100

    add form book interface

--- administrador/index.php
+++ administrador/index.php
@@ -34,7 +34,7 @@
                     <div class="card-body">
                         <form method="POST">
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Usuario</label>
+                                <label for="exampleInputEmail1">Usuario:</label>
                                 <input type="text" class="form-control" name='usuario' placeholder="Escribe tu usuario">
                                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                             </div>
