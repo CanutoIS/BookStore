d1d3eeb7d624543cedebe8380c3a40baa08a0537
CanutoIS <canutosorianoivan@gmail.com>
2024-03-02 18:46:19 +0100

    create form logic (add, modify, cancel)

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,4 +1,27 @@
 <?php include("../template/cabecera.php"); ?>
+<?php
+    $txtID = (isset($_POST['txtID'])) ? $_POST['txtID'] : '';
+    $txtNombre = (isset($_POST['txtNombre'])) ? $_POST['txtNombre'] : '';
+    $txtImagen = (isset($_FILES['txtImagen']['name'])) ? $_FILES['txtImagen']['name'] : '';
+    $accion = (isset($_POST['accion'])) ? $_POST['accion'] : '';
+
+    echo $txtID.'<br/>';
+    echo $txtNombre.'<br/>';
+    echo $txtImagen.'<br/>';
+    echo $accion.'<br/>';
+
+    switch($accion) {
+        case 'Agregar':
+            echo 'Presionado botón agregar';
+            break;
+        case 'Modificar':
+            echo 'Presionado botón modificar';
+            break;
+        case 'Cancelar':
+            echo 'Presionado botón cancelar';
+            break;
+    }
+?>
 
 <div class="col-md-5">
     <div class="card">
@@ -9,11 +32,11 @@
         <form method="POST" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="txtID">ID:</label>
-                <input type="text" class="form-control" name="txtId" id="txtID" placeholder="ID">
+                <input type="text" class="form-control" name="txtID" id="txtID" placeholder="ID">
             </div>
             <div class="form-group">
                 <label for="txtNombre">Nombre:</label>
-                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="ID">
+                <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre del libro">
             </div>
             <div class="form-group">
                 <label for="txtImagen">Imagen:</label>
@@ -22,16 +45,33 @@
             
 
             <div class="btn-group" role="group" aria-label="">
-                <button type="button" class="btn btn-success">Agregar</button>
-                <button type="button" class="btn btn-warning">Modificar</button>
-                <button type="button" class="btn btn-info">Cancelar</button>
+                <button type="submit" name="accion" value="Agregar" class="btn btn-success">Agregar</button>
+                <button type="submit" name="accion" value="Modificar" class="btn btn-warning">Modificar</button>
+                <button type="submit" name="accion" value="Cancelar" class="btn btn-info">Cancelar</button>
             </div>
         </form>
         </div>
     </div>
 </div>
 <div class="col-md-7">
-    Tabla de libros (mostrar los datos de los libros)
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nombre</th>
+                <th>Imagen</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2</td>
+                <td>Aprende de PHP</td>
+                <td>imagen.jpg</td>
+                <td>Seleccionar | Borrar</td>
+            </tr>
+        </tbody>
+    </table>
 </div>
 
 <?php include("../template/pie.php"); ?>
